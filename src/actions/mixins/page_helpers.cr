1545e8bb09b3f2703758b381f46b4dc3f1585456
Billy.Zheng <vil963@gmail.com>
2025-01-11 00:18:03 +0800

    Add PagerHelpers::Box, add #markdown, #para_text, #sub_title method

--- src/actions/mixins/page_helpers.cr
+++ src/actions/mixins/page_helpers.cr
@@ -1,38 +1,27 @@
 require "autolink"
+require "common_marker"
 
 module PageHelpers
-  def missing_ok(msg)
-    div class: "box ok" do
-      text msg
-    end
-  end
-
-  def missing_info(msg)
-    div class: "box info" do
-      strong "💡 小提示", class: "block titlebar"
-      text msg
-    end
-  end
-
-  def missing_warn(msg)
-    div class: "box warn" do
-      strong "警告", class: "block titlebar"
-      text msg
-    end
-  end
-
-  def missing_error(msg)
-    div class: "box bad" do
-      strong "错误", class: "block titlebar"
-      text msg
-    end
-  end
-
   def para_text(para, *, autolink = false)
     para = Autolink.auto_link(para) if autolink
 
     para do
       text para
     end
+  end
+
+  def sub_title(msg)
+    tag "sub-title" do
+      text msg
+    end
+  end
+
+  def markdown(text)
+    md = CommonMarker.new(
+      text,
+      options: ["unsafe"],
+      extensions: ["table", "strikethrough", "autolink", "tagfilter", "tasklist"]
+    )
+    raw md.to_html
   end
 end
