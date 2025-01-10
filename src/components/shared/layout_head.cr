21b5f51b636efc1d5969a10a46a960e4b008c2e4
Billy.Zheng <vil963@gmail.com>
2025-01-11 00:21:03 +0800

    refactor fontsawesome header

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -5,12 +5,17 @@ class Shared::LayoutHead < BaseComponent
     head do
       utf8_charset
       title "Crystal China - #{@page_title}"
+      if LuckyEnv.production?
+        js_link "https://kit.fontawesome.com/84b6da8eb9.js", crossorigin: "anonymous"
+      else
+        css_link "/css/all.css"
+      end
       css_link asset("css/app.css")
       js_link asset("js/app.js"), defer: "true"
-      js_link "https://kit.fontawesome.com/84b6da8eb9.js", crossorigin: "anonymous" if LuckyEnv.production?
+
       csrf_meta_tags
       responsive_meta_tag
-      css_link "https://fonts.googleapis.com/icon?family=Material+Icons"
+      # css_link "https://fonts.googleapis.com/icon?family=Material+Icons"
 
       # css_link "https://fonts.bunny.net/css?family=source-sans-3:400,700|m-plus-code-latin:400,700"
 
