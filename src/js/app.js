d419106a5920b0df7770c750fbbcb8e526743956
Billy.Zheng <vil963@gmail.com>
2025-01-13 18:05:32 +0800

    Refactor js use import

--- src/js/app.js
+++ src/js/app.js
@@ -6,45 +6,10 @@ import htmx from 'htmx.org';
 window.htmx = htmx;
 import _hyperscript from 'hyperscript.org';
 _hyperscript.browserInit();
+import { copyCodeButton } from './copy_code_button.js';
 
 import * as AsciinemaPlayer from 'asciinema-player';
 // AsciinemaPlayer.create('/demo.cast', document.getElementById('demo'));
-
-function copyCodeButton () {
-    const copyButtonLabel = "Copy Code";
-
-    // use a class selector if available
-    let blocks = document.querySelectorAll("pre.b");
-
-    blocks.forEach((block) => {
-        // only add button if browser supports Clipboard API
-        if (navigator.clipboard) {
-            let button = document.createElement("button");
-            button.className = "copyBtn";
-
-            button.innerText = copyButtonLabel;
-
-            block.prepend(button);
-
-            button.addEventListener("click", async () => {
-                await copyCode(block, button);
-            });
-        }
-    });
-
-    async function copyCode(block, button) {
-        let code = block.querySelector("code");
-        let text = code.innerText;
-
-        await navigator.clipboard.writeText(text);
-
-        button.innerText = "Code Copied";
-
-        setTimeout(() => {
-            button.innerText = copyButtonLabel;
-        }, 1500);
-    }
-}
 
 function init () {
     // htmx.logger = function (elt, event, data) {
@@ -52,8 +17,6 @@ function init () {
     //         console.log(event, elt, data);
     //     }
     // };
-
-    // console.log("111111111111111");
 
     // Delete 请求仍旧使用 form-encoded body 来传递参数。
     // htmx 2.0, 对于 DELETE 请求，将使用 params （根据 spec 规定）
---
+++ src/js/copy_code_button.js
@@ -0,0 +1,41 @@
+function copyCodeButton () {
+    const copyButtonLabel = "Copy Code";
+
+    // use a class selector if available
+    let blocks = document.querySelectorAll("pre.b");
+
+    blocks.forEach((block) => {
+        // only add button if browser supports Clipboard API
+        if (navigator.clipboard) {
+            let button = block.querySelector("button.copyBtn");
+
+            if (button === null) {
+                button = document.createElement("button");
+                button.className = "copyBtn";
+                button.innerText = copyButtonLabel;
+                button.addEventListener("click", async () => {
+                    await copyCode(block, button);
+                });
+
+                block.prepend(button);
+            }
+        }
+    });
+
+    async function copyCode(block, button) {
+        let code = block.querySelector("code");
+        let text = code.innerText;
+
+        await navigator.clipboard.writeText(text);
+
+        button.innerText = "Code Copied";
+
+        setTimeout(() => {
+            button.innerText = copyButtonLabel;
+        }, 1500);
+    }
+}
+
+export {
+    copyCodeButton
+}
