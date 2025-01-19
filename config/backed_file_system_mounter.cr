b80e1523ee3b09a7538d95252060bb9389684fa6
Billy.Zheng <vil963@gmail.com>
2025-01-19 19:38:26 +0800

    Mount src/pages/docs

--- config/backed_file_system_mounter.cr
+++ config/backed_file_system_mounter.cr
@@ -1,4 +1,4 @@
-BakedFileSystemMounter.assemble(["public"])
+BakedFileSystemMounter.assemble(["public", "src/pages/docs"])
 
 if LuckyEnv.production?
   STDERR.puts "Mounting from baked file system ..."
