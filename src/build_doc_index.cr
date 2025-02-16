8daff46dae19e54c93cb1135c1a71219dcf043aa
Billy.Zheng <vil963@gmail.com>
2025-02-17 02:09:03 +0800

    Added build_doc_index entry into shard.yml

---
+++ src/build_doc_index.cr
@@ -0,0 +1,16 @@
+require "json"
+require "./actions/mixins/page_helpers"
+
+File.open("public/docs/index.json", "w") do |file|
+  string = JSON.build(file) do |json|
+    json.array do
+      PageHelpers::PAGINATION_RELATION_MAPPING.each do |k, v|
+        json.object do
+          json.field "title", v[:title]
+          json.field "url", k
+          json.field "body", File.read("#{k.sub("/docs/", "markdowns/")}.md")
+        end
+      end
+    end
+  end
+end
