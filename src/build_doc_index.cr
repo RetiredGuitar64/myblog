088812f9bf5cd105aa2425d1f347d8c2d2fb56c1
Billy.Zheng <vil963@gmail.com>
2025-02-21 15:42:52 +0800

    Also pack svg files use brotli when update doc index

--- src/build_doc_index.cr
+++ src/build_doc_index.cr
@@ -1,8 +1,7 @@
 require "json"
-require "./actions/mixins/page_helpers"
 require "compress/gzip"
 require "brotli"
-require "file_utils"
+require "./actions/mixins/page_helpers"
 
 str = String.build do |io|
   io << "[input]\n"
@@ -17,58 +16,26 @@ end
 
 File.write("tmp/index.toml", str)
 
-if !Process.find_executable("wasm-pack")
-  abort "Install wasm-pack is required!
-Try: pacman -S wasm-pack if you use Arch Linux.
-"
-end
-
 system("bin/stork build --input tmp/index.toml --output public/docs/index.st")
 
-File.open("public/docs/stork.js", "r") do |input_file|
-  File.open("public/docs/stork.js.gz", "w") do |output_file|
-    Compress::Gzip::Writer.open(output_file) do |gz|
-      IO.copy(input_file, gz)
+Dir.glob(
+  "public/docs/stork.js",
+  "public/docs/stork.wasm",
+  "public/svgs/*.svg",
+).each do |file|
+  File.open(file, "r") do |input_file|
+    File.open("#{file}.gz", "w") do |output_file|
+      Compress::Gzip::Writer.open(output_file) do |gz|
+        IO.copy(input_file, gz)
+      end
     end
-  end
-end
 
-File.open("public/docs/stork.js", "r") do |input_file|
-  File.open("public/docs/stork.js.br", "w") do |output_file|
-    Compress::Brotli::Writer.open(output_file) do |br|
-      IO.copy(input_file, br)
-    end
-  end
-end
+    input_file.rewind
 
-File.open("public/docs/stork.wasm", "r") do |input_file|
-  File.open("public/docs/stork.wasm.gz", "w") do |output_file|
-    Compress::Gzip::Writer.open(output_file) do |gz|
-      IO.copy(input_file, gz)
-    end
-  end
-end
-
-File.open("public/docs/stork.wasm", "r") do |input_file|
-  File.open("public/docs/stork.wasm.br", "w") do |output_file|
-    Compress::Brotli::Writer.open(output_file) do |br|
-      IO.copy(input_file, br)
-    end
-  end
-end
-
-File.open("public/docs/index.st", "r") do |input_file|
-  File.open("public/docs/index.st.gz", "w") do |output_file|
-    Compress::Gzip::Writer.open(output_file) do |gz|
-      IO.copy(input_file, gz)
-    end
-  end
-end
-
-File.open("public/docs/index.st", "r") do |input_file|
-  File.open("public/docs/index.st.br", "w") do |output_file|
-    Compress::Brotli::Writer.open(output_file) do |br|
-      IO.copy(input_file, br)
+    File.open("#{file}.br", "w") do |output_file|
+      Compress::Brotli::Writer.open(output_file) do |br|
+        IO.copy(input_file, br)
+      end
     end
   end
 end
