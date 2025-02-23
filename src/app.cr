22801a8b87aec05152feb4c217445badd5457a65
Billy.Zheng <vil963@gmail.com>
2025-02-23 18:49:35 +0800

    Add built date into version

--- src/app.cr
+++ src/app.cr
@@ -3,7 +3,8 @@ module App
               `shards version "#{__DIR__}"`.chomp.stringify +
               " (rev " +
               `git rev-parse --short HEAD`.chomp.stringify +
-              ")"
+              ")" +
+              `date '+ %Y-%m-%d %H:%M:%S'`.chomp.stringify
             }}
 end
 
