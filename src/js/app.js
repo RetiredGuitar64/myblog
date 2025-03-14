850f946d248bb9387d5bf8f0d234ce76423a051c
Billy.Zheng <vil963@gmail.com>
2025-03-14 23:21:12 +0800

    add comment

--- src/js/app.js
+++ src/js/app.js
@@ -12,26 +12,26 @@ import tabs from 'missing.css/www/missing-js/tabs.js';
 // AsciinemaPlayer.create('/demo.cast', document.getElementById('demo'));
 
 function init () {
-//     // htmx.logger = function (elt, event, data) {
-//     //     if (console) {
-//     //         console.log(event, elt, data);
-//     //     }
-//     // };
+    // htmx.logger = function (elt, event, data) {
+    //     if (console) {
+    //         console.log(event, elt, data);
+    //     }
+    // };
 
-//     // Delete 请求仍旧使用 form-encoded body 来传递参数。
-//     // htmx 2.0, 对于 DELETE 请求，将使用 params （根据 spec 规定）
-//     // 这里设定，仅仅 get 请求使用 params
+    // Delete 请求仍旧使用 form-encoded body 来传递参数。
+    // htmx 2.0, 对于 DELETE 请求，将使用 params （根据 spec 规定）
+    // 这里设定，仅仅 get 请求使用 params
     htmx.config.methodsThatUseUrlParams = ['get'];
-//     // 2.0 不允许使用 htmx 执行 cross-domain requests.
-//     // 取消注释来允许它正常发送请求。
-//     // htmx.config.selfRequestsOnly = false;
+    // 2.0 不允许使用 htmx 执行 cross-domain requests.
+    // 取消注释来允许它正常发送请求。
+    // htmx.config.selfRequestsOnly = false;
 
-//     // setIPhoneDataAttribute();
-//     // startLogoAnimation();
+    // setIPhoneDataAttribute();
+    // startLogoAnimation();
 
     copyCodeButton();
 
-//     // 确保下面的函数，只在 body 重新改变时才触发
+    // 确保下面的函数，只在 body 重新改变时才触发
     if (event.detail.elt.nodeName == "BODY") {
         stork.initialize("https://assets.crystal-china.org/docs/stork.wasm");
         stork.downloadIndex(
