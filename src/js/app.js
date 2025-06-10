2232569dec78ba001c10c82955e775851d06242c
Billy.Zheng <vil963@gmail.com>
2025-06-11 01:59:44 +0800

    add console.log output mixManifest again

--- src/js/app.js
+++ src/js/app.js
@@ -25,6 +25,7 @@ function init() {
 
     // 确保下面的函数，只在 body 重新改变时才触发
     if (event.detail.elt.nodeName == "BODY") {
+        console.log(mixManifest);
         stork.initialize(
             `${assetHost}${mixManifest["/docs/stork.wasm"] ?? "/docs/stork.wasm"}`,
         );
