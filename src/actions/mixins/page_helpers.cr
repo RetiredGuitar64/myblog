a600cd95a21e6eb2ec24f2b1019253194c3b3a8a
Billy.Zheng <vil963@gmail.com>
2025-06-11 23:15:46 +0800

    Refactor to remove the default parent in page mapping

--- src/actions/mixins/page_helpers.cr
+++ src/actions/mixins/page_helpers.cr
@@ -3,21 +3,21 @@ require "digest/md5"
 
 module PageHelpers
   PAGINATION_RELATION_MAPPING = {
-    "/docs/index"                                    => {title: "前言", sub_title: "写在开始之前", parent: "root"},
-    "/docs/introduction"                             => {title: "简介", sub_title: "", parent: "root"},
-    "/docs/install"                                  => {title: "安装", sub_title: "", parent: "root"},
+    "/docs/index"                                    => {title: "前言", sub_title: "写在开始之前"},
+    "/docs/introduction"                             => {title: "简介", sub_title: ""},
+    "/docs/install"                                  => {title: "安装", sub_title: ""},
     "/docs/package_manager"                          => {title: "包管理", sub_title: "shards 命令", parent: "/docs/install"},
-    "/docs/for_advanced_rubyists"                    => {title: "写给 Rubyists", sub_title: "分类讨论 Crystal 和 Ruby 的异同", parent: "root"},
+    "/docs/for_advanced_rubyists"                    => {title: "写给 Rubyists", sub_title: "分类讨论 Crystal 和 Ruby 的异同"},
     "/docs/for_advanced_rubyists/type"               => {title: "类型", sub_title: "", parent: "/docs/for_advanced_rubyists"},
     "/docs/for_advanced_rubyists/method"             => {title: "方法", sub_title: "", parent: "/docs/for_advanced_rubyists"},
     "/docs/for_advanced_rubyists/block"              => {title: "代码块", sub_title: "", parent: "/docs/for_advanced_rubyists"},
     "/docs/for_advanced_rubyists/misc"               => {title: "杂项", sub_title: "", parent: "/docs/for_advanced_rubyists"},
     "/docs/for_advanced_rubyists/performance"        => {title: "性能因素", sub_title: "", parent: "/docs/for_advanced_rubyists"},
     "/docs/for_advanced_rubyists/migrate_to_crystal" => {title: "迁移 Ruby 代码到 Crystal", sub_title: "", parent: "/docs/for_advanced_rubyists"},
-    "/docs/basic"                                    => {title: "基础知识", sub_title: "一些基础知识的简单总结", parent: "root"},
-    "/docs/profile"                                  => {title: "查找性能瓶颈 (WIP)", sub_title: "", parent: "root", hidden: "true"},
-    "/docs/cross_compile"                            => {title: "交叉编译", sub_title: "", parent: "root"},
-    "/docs/concurrency"                              => {title: "并发原语", sub_title: "", parent: "root"},
+    "/docs/basic"                                    => {title: "基础知识", sub_title: "一些基础知识的简单总结"},
+    "/docs/profile"                                  => {title: "查找性能瓶颈 (WIP)", sub_title: "", hidden: "true"},
+    "/docs/cross_compile"                            => {title: "交叉编译", sub_title: ""},
+    "/docs/concurrency"                              => {title: "并发原语", sub_title: ""},
     "/docs/concurrency/execution_context"            => {title: "执行上下文(WIP)", sub_title: "", parent: "/docs/concurrency"},
   }
   PAGINATION_URLS = PAGINATION_RELATION_MAPPING.keys
@@ -33,7 +33,7 @@ module PageHelpers
   SIDEBAR_LINKS = {} of String => PageMapping
 
   PAGINATION_RELATION_MAPPING.each do |k, v|
-    parent = v[:parent]
+    parent = v[:parent]? || "root"
     hidden = v[:hidden]?
 
     if parent == "root"
