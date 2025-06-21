a84bb7b29a162b60da4a3169c25e5bd8d9d602c6
Billy.Zheng <vil963@gmail.com>
2025-06-21 23:38:13 +0800

    use X icon svg

--- src/components/footer.cr
+++ src/components/footer.cr
@@ -8,8 +8,8 @@ class Footer < BaseComponent
       a href: "https://github.com/crystal-china", target: "_blank", rel: "nofollow", title: "本站在 GitHub 上面的开源内容" do
         img src: asset("svgs/github-icon.svg"), alt: "github"
       end
-      a href: "https://x.com/crystalchinaorg", target: "_blank", rel: "nofollow", title: "本站的 Twitter 账号" do
-        img src: asset("svgs/twitter-icon.svg"), alt: "twitter"
+      a href: "https://x.com/crystalchinaorg", target: "_blank", rel: "nofollow", title: "本站的 X 账号" do
+        img src: asset("svgs/x-icon.svg"), alt: "x.com"
       end
       a href: "https://crystal-lang.org/", target: "_blank", rel: "nofollow", title: "Crystal 官方网站" do
         img src: asset("svgs/crystal-lang-icon.svg"), alt: "crystal-lang"
