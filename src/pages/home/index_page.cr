a25597521e2fe69ed1f4f4fcc2b840039a76e9a1
Billy.Zheng <vil963@gmail.com>
2025-06-21 23:38:18 +0800

    Added links

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -18,5 +18,43 @@ class Home::IndexPage < MainLayout
         style: "cursor:move",
       )
     end
+
+    div class: "f-row justify-content:space-around" do
+      ul class: "align-items:stretch" do
+        li do
+          a "Crystal 官方网站（英文）", href: "https://www.crystal-lang.org"
+        end
+        li do
+          a "Crystal Github", href: "https://github.com/crystal-lang"
+        end
+        li do
+          a "Crystal China Github", href: "https://github.com/crystal-china"
+        end
+        li do
+          a "Crystal 官方社区（英文）", href: "https://forum.crystal-lang.org"
+        end
+        li do
+          a "Discord（英文）", href: "https://discord.gg/YS7YvQy"
+        end
+      end
+
+      ul class: "align-items:stretch" do
+        li do
+          a "API 文档（英文）", href: "https://crystal-lang.org/api/latest/"
+        end
+        li do
+          a "shards.info (shard 搜索)", href: "https://shards.info/"
+        end
+        li do
+          a "shardbox.org(shard 搜索)", href: "https://shardbox.org/"
+        end
+        li do
+          a "Awesome Crystal", href: "https://github.com/veelenga/awesome-crystal"
+        end
+        li do
+          a "r/crystal_programming（英文）", href: "https://www.reddit.com/r/crystal_programming/"
+        end
+      end
+    end
   end
 end
