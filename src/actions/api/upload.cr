1cc2b99cca6f141ab2d950c2308873b5eb10a824
Billy.Zheng <vil963@gmail.com>
2025-06-23 01:06:21 +0800

    Use IO.pipe replace IO::Memory for less memory usage.

--- src/actions/api/upload.cr
+++ src/actions/api/upload.cr
@@ -1,22 +1,29 @@
 class Api::Upload < ApiAction
   post "/api/upload" do
     source = params.from_multipart.last["source"]
-    io = IO::Memory.new
+
+    # use IO.pipe instead of IO::Memory to reduce memory usage.
+    # https://forum.crystal-lang.org/t/upload-image-failed-use-http-client-but-test-with-postman-work/8171/13
+
+    reader, writer = IO.pipe
     headers = HTTP::Headers.new
-    form_data = HTTP::FormData::Builder.new(io)
+    form_data = HTTP::FormData::Builder.new(writer)
     headers["Content-Type"] = form_data.content_type
 
-    file = File.open(source.path) do |file|
-      form_data.field("key", FREEIMAGE_HOST_API_KEY)
-      form_data.file("source", file, HTTP::FormData::FileMetadata.new(filename: source.filename))
-      form_data.finish
+    spawn do
+      file = File.open(source.path) do |file|
+        form_data.field("key", FREEIMAGE_HOST_API_KEY)
+        form_data.file("source", file, HTTP::FormData::FileMetadata.new(filename: source.filename))
+        form_data.finish
+      end
+    ensure
+      writer.close
     end
 
-    p! headers
     response = HTTP::Client.post(
       url: "https://freeimage.host/api/1/upload",
       headers: headers,
-      body: io.rewind
+      body: reader
     )
 
     body = JSON.parse(response.body)
