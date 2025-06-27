94ac0de5beef6fbe4b29deb86655d8f126760c26
Billy.Zheng <vil963@gmail.com>
2025-06-27 18:17:02 +0800

    filename rename

--- src/actions/docs/htmx/reply.cr
+++ src/actions/docs/htmx/reply.cr
@@ -1,28 +1,49 @@
-class Docs::Htmx::Reply < DocAction
-  param user_id : Int64
-  param doc_id : Int64
-  param content : String
-  param doc_path : String
+module Docs
+  module Htmx
+    class Reply < DocAction
+      param user_id : Int64
+      param doc_id : Int64
+      param content : String
+      param doc_path : String
 
-  # 回复评论按钮
-  post "/docs/htmx/reply" do
-    me = current_user
-    return head 401 if me.nil?
-    return head 401 if user_id != me.id
-    return head 400 if content.blank?
+      # 回复评论按钮
+      post "/docs/htmx/reply" do
+        me = current_user
+        return head 401 if me.nil?
+        return head 401 if user_id != me.id
+        return head 400 if content.blank?
 
-    SaveReply.create(user_id: user_id, doc_id: doc_id, content: content) do |op, _saved_reply|
-      if op.saved?
-        component(
-          Docs::FormWithReplies,
-          formatter: formatter,
-          pagination: replies_pagination(doc_path: doc_path),
-          current_user: me,
-          doc_path: doc_path,
-          order_by: "desc"
-        )
-      else
-        head 400
+        SaveReply.create(user_id: user_id, doc_id: doc_id, content: content) do |op, _saved_reply|
+          if op.saved?
+            component(
+              Docs::FormWithReplies,
+              formatter: formatter,
+              pagination: replies_pagination(doc_path: doc_path),
+              current_user: me,
+              doc_path: doc_path,
+              order_by: "desc"
+            )
+          else
+            head 400
+          end
+        end
+      end
+    end
+
+    class ReplyDelete < DocAction
+      param user_id : Int64
+      param reply_id : Int64
+
+      delete "/docs/htmx/reply" do
+        me = current_user
+        return head 401 if me.nil?
+        return head 401 if user_id != me.id
+
+        reply = ReplyQuery.find(reply_id)
+
+        ::DeleteReply.delete!(reply)
+
+        head 200
       end
     end
   end
