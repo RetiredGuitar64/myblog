367e17e534bdae7ec878937198836a6ca2188571
Billy.Zheng <vil963@gmail.com>
2025-07-03 01:16:25 +0800

    merge db schema

--- db/migrations/00000000000001_create_users.cr
+++ db/migrations/00000000000001_create_users.cr
@@ -8,7 +8,7 @@ class CreateUsers::V00000000000001 < Avram::Migrator::Migration::V1
       add name : String, unique: true
       add avatar : String?
       add email : String, unique: true, case_sensitive: false
-      add encrypted_password : String
+      add encrypted_password : String?
     end
   end
 
--- db/migrations/20250702091600_change_password_optional_for_user.cr
+++
@@ -1,5 +0,0 @@
-class ChangePasswordOptionalForUser::V20250702091600 < Avram::Migrator::Migration::V1
-  def migrate
-    make_optional table_for(User), :encrypted_password
-  end
-end
