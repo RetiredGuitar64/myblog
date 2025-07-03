40018296b3c28b1601dce395d744194e049c4eda
Billy.Zheng <vil963@gmail.com>
2025-07-03 15:20:22 +0800

    Disable table cell if hours expired.

--- src/components/table_scheduler_cell.cr
+++ src/components/table_scheduler_cell.cr
@@ -6,21 +6,24 @@ class TableSchedulerCell < BaseComponent
 
   def render
     me = current_user
+    cell_hour = Time.parse_local("#{date} #{hour}:59", "%Y-%m-%d %H:%M")
+    opts = {
+      class: cell_hour > Time.local ? "" : "disabled",
+    }
 
     if me && me.email == ENV["ADMIN_EMAIL"]?
-      opts = {
-        hx_post:    Htmx::HourlySchedule.path_without_query_params,
-        hx_include: "[name='_csrf']",
-        hx_prompt:  "修改预约（#{date} #{hour}:00）",
-        hx_swap:    "outerHTML",
-        hx_vals:    "{\"date\": \"#{date}\", \"hour\": \"#{hour}\"}",
-      }
+      opts = opts.merge(
+        {
+          hx_post:    Htmx::HourlySchedule.path_without_query_params,
+          hx_include: "[name='_csrf']",
+          hx_prompt:  "修改预约（#{date} #{hour}:00）",
+          hx_swap:    "outerHTML",
+          hx_vals:    "{\"date\": \"#{date}\", \"hour\": \"#{hour}\"}",
+        })
 
       opts = opts.merge(data_tooltip: comment.to_s) if comment.present?
-
-      td(available? ? "🟢" : "🔴", opts)
-    else
-      td(available? ? "🟢" : "🔴")
     end
+
+    td(available? ? "🟢" : "🔴", opts)
   end
 end
