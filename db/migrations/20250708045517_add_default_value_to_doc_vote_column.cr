e929fba23138ace0c5d0fb623b129053a49aad19
Billy.Zheng <vil963@gmail.com>
2025-07-08 13:00:12 +0800

    add_default value to doc vote column

---
+++ db/migrations/20250708045517_add_default_value_to_doc_vote_column.cr
@@ -0,0 +1,12 @@
+class AddDefaultValueToDocVoteColumn::V20250708045517 < Avram::Migrator::Migration::V1
+  def migrate
+    alter table_for(Doc) do
+      change_default votes : Time, default: JSON.parse({"👍" => 0, "👎" => 0, "❤️" => 0}.to_json
+    end
+
+  end
+
+  def rollback
+    # drop table_for(Thing)
+  end
+end
