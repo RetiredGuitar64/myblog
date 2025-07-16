5db7777e2c9af218dc3f8f3eaab02e22114bd803
Billy.Zheng <vil963@gmail.com>
2025-07-17 02:18:52 +0800

    Add belongs_to_mounter column to replies table.

--- db/migrations/20250716093250_add_belongs_to_reply_for_replies.cr
+++ db/migrations/20250716093250_add_belongs_to_reply_for_replies.cr
@@ -2,12 +2,14 @@ class AddBelongsToReplyForReplies::V20250716093250 < Avram::Migrator::Migration:
   def migrate
     alter table_for(Reply) do
       add_belongs_to reply : Reply?, on_delete: :cascade
+      add belongs_to_counter : Int32, default: 0
     end
   end
 
   def rollback
     alter table_for(Reply) do
       remove_belongs_to :reply
+      remove :belongs_to_counter
     end
   end
 end
