59afbd40d3c722a2bbab80fad26bb8b1e1a00137
Billy.Zheng <vil963@gmail.com>
2025-08-01 01:39:34 +0800

    Try tailwindcss

--- src/components/footer.cr
+++ src/components/footer.cr
@@ -1,6 +1,6 @@
 class Footer < BaseComponent
   def render
-    section class: "tool-bar", style: "margin-top: 10px;" do
+    section class: "mt-4 flex flex-row flex-wrap gap-4" do
       text "Crystal China"
       a "spider.yuxuan@gmail.com", href: "mailto:spider.yuxuan@gmail.com"
       span(
@@ -11,14 +11,17 @@ class Footer < BaseComponent
         text "在线用户 #{ONLINE_USER_COUNTER.keys.size} 人, 游客 #{ONLINE_IP_COUNTER.keys.size} 人"
       end
       hr "aria-orientation": "vertical"
-      a href: "https://github.com/RetiredGuitar64/myblog", target: "_blank", rel: "nofollow", title: "本站在 GitHub 上面的开源内容" do
-        img src: asset("svgs/github-icon.svg"), alt: "github"
-      end
-      a href: "https://x.com/e5YxtF6E1QCammW", target: "_blank", rel: "nofollow", title: "我的 X 账号" do
-        img src: asset("svgs/x-icon.svg"), alt: "x.com"
-      end
-      a href: "https://crystal-lang.org/", target: "_blank", rel: "nofollow", title: "Crystal 官方网站" do
-        img src: asset("svgs/crystal-lang-icon.svg"), alt: "crystal-lang"
+
+      span class: "flex flex-row" do
+        a href: "https://github.com/RetiredGuitar64/myblog", target: "_blank", rel: "nofollow", title: "本站在 GitHub 上面的开源内容" do
+          img src: asset("svgs/github-icon.svg"), alt: "github"
+        end
+        a href: "https://x.com/e5YxtF6E1QCammW", target: "_blank", rel: "nofollow", title: "我的 X 账号" do
+          img src: asset("svgs/x-icon.svg"), alt: "x.com"
+        end
+        a href: "https://crystal-lang.org/", target: "_blank", rel: "nofollow", title: "Crystal 官方网站" do
+          img src: asset("svgs/crystal-lang-icon.svg"), alt: "crystal-lang"
+        end
       end
     end
   end
